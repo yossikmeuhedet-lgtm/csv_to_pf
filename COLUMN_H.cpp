e63124cdaf2bad02f3429164b3ac17604dcf6ec9
yossikmeuhedet-lgtm <yossi.k.meuhedet@gmail.com>
2026-07-01 16:37:20 +0300

    Update COLUMN_H.cpp
    working version

--- COLUMN_H.cpp
+++ COLUMN_H.cpp
@@ -13,17 +13,23 @@ class Column {
     private:
         std::string name;
         size_t width;
+        size_t precision;
+        size_t wholePart;
         DataType type;
         const std::string DataTypeName(DataType t) const;
-        static std::string Trim(const std::string& s);
     public:
         Column(std::string name, size_t width = 0, DataType type = INTEGER);
         void setWidth(size_t width);
         const size_t getWidth() const;
+        void setPrecision(size_t precision);
+        const size_t getPrecision() const;
         void setType(DataType type);
         const DataType getType() const;
+        const size_t getWholePart() const;
         static DataType DetectValueType(const std::string& input);
+        static size_t DetectPrecision(const std::string& input);
         std::string columnInfo() const;
+        static std::string Trim(const std::string& s);
 };
 
 #endif
